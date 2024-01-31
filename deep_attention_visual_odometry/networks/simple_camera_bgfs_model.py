Fix so many little bugs, mostly tensor shapes. Doesn't quite update yet, there's a problem with the masked gradient computation and slicing.

deep_attention_visual_odometry/networks/simple_camera_bgfs_model.py
@@ -1,18 +1,47 @@
 import torch
 import torch.nn as nn
-from deep_attention_visual_odometry.solvers.camera_model import SimpleCameraModel, SimpleCameraModelInitialGuess
+from deep_attention_visual_odometry.solvers.camera_model import (
+    SimpleCameraModel,
+    SimpleCameraModelInitialGuess,
+)
+from deep_attention_visual_odometry.solvers.line_search_strong_wolfe_conditions import (
+    LineSearchStrongWolfeConditions,
+)
 from deep_attention_visual_odometry.solvers import BFGSCameraSolver
 
 
 class SimpleCameraBGFSModel(nn.Module):
-
-    def __init__(self, num_views: int, num_points: int, max_iterations: int,):
+    def __init__(
+        self,
+        num_views: int,
+        num_points: int,
+        max_iterations: int,
+        epsilon: float,
+        max_step_size: float,
+        zoom_iterations: int,
+        sufficient_decrease: float,
+        curvature: float,
+    ):
         super().__init__()
         num_parameters = 3 + 6 * num_views + 3 * num_points
-        self.initial_guess = SimpleCameraModelInitialGuess(num_views)
-        self.solver = BFGSCameraSolver(max_iterations=max_iterations, num_parameters=num_parameters)
+        self.initial_guess = SimpleCameraModelInitialGuess(num_views, num_points)
+        self.line_search = LineSearchStrongWolfeConditions(
+            max_step_size=max_step_size,
+            zoom_iterations=zoom_iterations,
+            sufficient_decrease=sufficient_decrease,
+            curvature=curvature,
+        )
+        self.solver = BFGSCameraSolver(
+            max_iterations=max_iterations,
+            num_parameters=num_parameters,
+            epsilon=epsilon,
+            line_search=self.line_search,
+        )
 
-    def forward(self, projected_points: torch.Tensor, world_points: torch.Tensor) -> SimpleCameraModel:
-        initial_guess = self.initial_guess(projected_points, world_points)
+    def forward(
+        self, projected_points: torch.Tensor
+    ) -> SimpleCameraModel:
+        projected_points = projected_points.unsqueeze(1)
+        initial_guess = self.initial_guess(projected_points)
         solution = self.solver(initial_guess)
         return solution

deep_attention_visual_odometry/solvers/bfgs_camera_solver.py
@@ -56,7 +56,8 @@ class BFGSCameraSolver(nn.Module):
         for step in range(self.max_iterations):
             # Compute a search direction as -H \delta f
             gradient = function.get_gradient()
-            search_direction = -1 * torch.matmul(inverse_hessian, gradient)
+            search_direction = -1.0 * torch.matmul(inverse_hessian, gradient.unsqueeze(-1))
+            search_direction = search_direction.squeeze(-1)
             # Line search for an update step that satisfies the wolfe conditions
             next_function_point, step = self.line_search(function, search_direction)
             # Update the inverse hessian based on the next chosen point

deep_attention_visual_odometry/solvers/camera_model/simple_camera_model.py
@@ -153,7 +153,7 @@ class SimpleCameraModel(IOptimisableFunction):
             )
             self._gradient_mask = None
         elif self._gradient_mask is not None:
-            camera_relative_points = self._camera_relative_points()
+            camera_relative_points = self._get_camera_relative_points()
             camera_relative_points = camera_relative_points[self._gradient_mask]
             u = self._get_u()
             u = u[self._gradient_mask]
@@ -206,10 +206,10 @@ class SimpleCameraModel(IOptimisableFunction):
         y_params = parameters[:, :, y_idx:z_idx]
         z_params = parameters[:, :, z_idx:end_idx]
 
-        t_params = torch.stack([tx_params, ty_params, tz_params], dim=2)
-        point_params = torch.stack([x_params, y_params, z_params], dim=2)
+        t_params = torch.stack([tx_params, ty_params, tz_params], dim=-1)
+        point_params = torch.stack([x_params, y_params, z_params], dim=-1)
         new_orientation = self._orientation.add_lie_parameters(
-            torch.stack([a_params, b_params, c_params], dim=2)
+            torch.stack([a_params, b_params, c_params], dim=-1).unsqueeze(-2)
         )
         return type(self)(
             focal_length=self._focal_length + parameters[:, :, self.F],

deep_attention_visual_odometry/solvers/camera_model/simple_camera_model_initial_guess.py
@@ -1,33 +1,37 @@
 import torch
 import torch.nn as nn
 from .simple_camera_model import SimpleCameraModel
+from .lie_rotation import LieRotation
 
 
 class SimpleCameraModelInitialGuess(nn.Module):
 
-    def __init__(self, num_views: int):
+    def __init__(self, num_views: int, num_points: int):
         super().__init__()
         self.focal_length = nn.Parameter(torch.tensor([[60.0]]))
         self.cx = nn.Parameter(torch.tensor([[320.]]))
         self.cy = nn.Parameter(torch.tensor([[240.]]))
-        self.rot_forward = nn.Parameter(torch.tensor([1., 0., 0.]).reshape(1, 1, 1, 3).tile(1, 1, num_views, 1))
-        self.rot_up = nn.Parameter(torch.tensor([0., 1., 0.]).reshape(1, 1, 1, 3).tile(1, 1, num_views, 1))
-        self.translation = nn.Parameter(torch.zeros(1, 1, num_views, 3))
+        self.orientation = nn.Parameter(torch.tensor([0, 0., 0.]).reshape(1, 1, 1, 1, 3).tile(1, 1, num_views, 1, 1))
+        translation = torch.zeros(1, 1, num_views, 3)
+        translation[:, :, :, 2] = 10.0
+        self.translation = nn.Parameter(translation)
+        world_points = torch.randn(1, 1, num_points, 3)
+        world_points[0, 0, 0, :] = 0.0
+        self.world_points = nn.Parameter(world_points)
 
-    def forward(self, projected_points: torch.Tensor, world_points: torch.Tensor) -> SimpleCameraModel:
+    def forward(self, projected_points: torch.Tensor) -> SimpleCameraModel:
         batch_size = projected_points.size(0)
         focal_length = self.focal_length.tile(batch_size, 1)
         cx = self.cx.tile(batch_size, 1)
         cy = self.cy.tile(batch_size, 1)
-        a = self.rot_forward.tile(batch_size, 1, 1, 1)
-        b = self.rot_up.tile(batch_size, 1, 1, 1)
+        orientation = self.orientation.tile(batch_size, 1, 1, 1, 1)
         translation = self.translation.tile(batch_size, 1, 1, 1)
+        world_points = self.world_points.tile(batch_size, 1, 1, 1)
         return SimpleCameraModel(
             focal_length=focal_length,
             cx=cx,
             cy=cy,
-            a=a,
-            b=b,
+            orientation=LieRotation(orientation),
             translation=translation,
             world_points=world_points,
             true_projected_points=projected_points