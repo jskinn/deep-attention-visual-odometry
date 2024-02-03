Rewrite the bgfs inverse hessian update, the shapes are now correct, the loop can iterate.

deep_attention_visual_odometry/solvers/bfgs_camera_solver.py
@@ -52,43 +52,120 @@ class BFGSCameraSolver(nn.Module):
         inverse_hessian = self.inv_hessian.tile(batch_size, num_estimates, 1, 1)
 
         # TODO: Use updating as a mask to reduce computation
-        updating = torch.ones(batch_size, num_estimates, device=function.device)
-        for step in range(self.max_iterations):
+        updating = torch.ones(
+            batch_size, num_estimates, device=function.device, dtype=torch.bool
+        )
+        for step_idx in range(self.max_iterations):
             # Compute a search direction as -H \delta f
             gradient = function.get_gradient()
-            search_direction = -1.0 * torch.matmul(inverse_hessian, gradient.unsqueeze(-1))
-            search_direction = search_direction.squeeze(-1)
+            if step_idx == 0:
+                search_direction = -1.0 * gradient
+            else:
+                search_direction = -1.0 * torch.matmul(
+                    inverse_hessian, gradient.unsqueeze(-1)
+                )
+                search_direction = search_direction.squeeze(-1)
             # Line search for an update step that satisfies the wolfe conditions
             next_function_point, step = self.line_search(function, search_direction)
             # Update the inverse hessian based on the next chosen point
             # This is expressed as one update equation in the BFGS algorithm,
             # but it's got too many terms for one line.
             delta_gradient = next_function_point.get_gradient() - gradient
-            step_dot_delta_grad = torch.sum(step * delta_gradient, dim=-1)
-            inv_hessian_times_delta_grad = (
-                delta_gradient[:, :, None, :]
-                @ inverse_hessian
-                @ delta_gradient[:, :, :, None]
+            if step_idx == 0:
+                # For the first step, initialize the inverse hessian
+                inverse_hessian = estimate_initial_inverse_hessian(
+                    gradient.size(2), step, delta_gradient
+                )
+            inverse_hessian = update_inverse_hessian(
+                inverse_hessian, step, delta_gradient
             )
-            step_outer_product = step[:, :, :, None] @ step[:, :, None, :]
-            inv_hessian_delta_grad_step = (
-                inverse_hessian @ delta_gradient[:, :, :, None] @ step[:, :, None, :]
-            )
-            step_delta_grad_inv_hessian = (
-                step[:, :, :, None] @ delta_gradient[:, :, None, :] @ inverse_hessian
-            )
-            delta_inv_hessian = torch.zeros_like(inverse_hessian)
-            delta_inv_hessian[step_dot_delta_grad.squeeze(dim=(2, 3)) != 0] = (
-                step_outer_product
-                * (step_dot_delta_grad + inv_hessian_times_delta_grad)
-                / step_dot_delta_grad.square()
-                - (inv_hessian_delta_grad_step + step_delta_grad_inv_hessian)
-                / step_dot_delta_grad
-            )
-            inverse_hessian = inverse_hessian + delta_inv_hessian
             # Set the current evaluation point to the next one
             function = function.masked_update(next_function_point, updating)
             updating = torch.logical_and(
                 updating, torch.greater(function.get_error(), self.epsilon)
             )
         return function
+
+
+def estimate_initial_inverse_hessian(
+    ndim: int, step: torch.Tensor, delta_gradient: torch.Tensor
+) -> torch.Tensor:
+    """
+    Choose an initial value for the inverse hessian approximation.
+    H_0 = \frac{y^T s}{y^T y} I
+    This is equation 6.20 from "Numerical Optimisation" by Nocedal and Wright 2009.
+    :param ndim: The number of parameters
+    :param step: The first chosen step, satisfying the wolffe conditions.
+    :param delta_gradient: The change in gradient between the current and next iterate points.
+    :return: An initial guess for the inverse hessian H_0, to be updated to H_1.
+    """
+    scale = (step * delta_gradient).sum(dim=-1)
+    scale = scale / delta_gradient.square().sum(dim=-1)
+    return scale[:, :, None, None] * torch.eye(
+        ndim, device=step.device, dtype=step.dtype
+    ).reshape(1, 1, ndim, ndim)
+
+
+def update_inverse_hessian(
+    inverse_hessian: torch.Tensor, step: torch.Tensor, delta_gradient: torch.Tensor
+) -> torch.Tensor:
+    """
+    Calculate the BFGS update to the inverse hessian:
+    H_{+} = (I - \frac{s y^T}{y^T s})H(I - frac{y s^T}{y^T s^T}) + frac{s s^T}{y^T s}
+    (from equation 6.17, "Numerical Optimisation", Nocedal and Wright 2009)
+
+    :param inverse_hessian: The current inverse hessian estimate H
+    :param step: The change in parameters, x_{t+1} - x_{t}
+    :param delta_gradient: The change in gradient f(x_{t+1}) - f(x_{t})
+    :return: The updated inverse hessian estimate, H_{+}
+    """
+    # = (H - \frac{1}{y^T s} s y^T H)(I - frac{y s^T}{y^T s^T}) + frac{s s^T}{y^T s}
+    # = H - frac{1}{y^T s} H y s^T - \frac{1}{y^T s} s y^T H + \frac{1}{(y^T s)^2} s y^T H y s^T + frac{s s^T}{y^T s}
+    # = H - frac{1}{y^T s} (H y s^T + s y^T H) + \frac{1}{y^T s} (\frac{y^T H y}{y^T s} + 1.0) s s^T
+    # y^T s (curvature):
+    curvature = torch.sum(step * delta_gradient, dim=-1)
+    # Curvature (this product) _should_ be strictly positive.
+    # If it isn't, set this scale factor to 0, so that the update is skipped
+    inv_curvature = torch.where(
+        torch.greater(curvature, 0.0), 1.0 / curvature, torch.zeros_like(curvature)
+    )
+    # y^T H y / (y^T s):
+    # For numerical stability, we assume |y^T H| < |y| and |y / (y^T s)| < |y|
+    # So we calcualte those ratios first before multiplying
+    # To avoid an intermediate scale ~|y|^2
+    gradient_postmultiply_hessian = torch.matmul(
+        delta_gradient[:, :, None, :], inverse_hessian
+    )
+    gradient_on_curvature = delta_gradient * inv_curvature[:, :, None]
+    gradient_inner_product = (
+        gradient_postmultiply_hessian * gradient_on_curvature[:, :, None, :]
+    ).sum(dim=-1)
+    # s s^T / (y^T s):
+    # Again, to avoid a matrix scaled ~|s^2|, we scale first by 1/|y^T s|
+    step_on_curvature = step * inv_curvature[:, :, None]
+    step_outer_product = torch.matmul(
+        step_on_curvature[:, :, :, None], step[:, :, None, :]
+    )
+    # \frac{1}{y^T s} (\frac{y^T H y}{y^T s} + 1.0) s s^T
+    step_outer_product = step_outer_product * (
+        1.0 + gradient_inner_product[:, :, :, None]
+    )
+
+    # \frac{1}{y^T s} s y^T H = \frac{s}{y^T s} (y^T H)
+    step_gradient_product = torch.matmul(
+        step_on_curvature[:, :, :, None], gradient_postmultiply_hessian
+    )
+    # frac{1}{y^T s} H y s^T = (H y) frac{s^T}{y^T s}
+    gradient_premultiply_hessian = torch.matmul(
+        inverse_hessian, delta_gradient[:, :, :, None]
+    )
+    gradient_step_product = torch.matmul(
+        gradient_premultiply_hessian, step_on_curvature[:, :, None, :]
+    )
+
+    return (
+        inverse_hessian
+        + step_outer_product
+        - step_gradient_product
+        - gradient_step_product
+    )