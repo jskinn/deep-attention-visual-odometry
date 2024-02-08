4e4544cbb2c84c9379b771860adbe3eb264e82de
John Skinner <jskinn7@gmail.com>
2024-02-08 21:13:46 +1000

    Update the initial guess for the camera model to use the shortened number of world points. Black reformat.

--- deep_attention_visual_odometry/solvers/camera_model/simple_camera_model_initial_guess.py
+++ deep_attention_visual_odometry/solvers/camera_model/simple_camera_model_initial_guess.py
@@ -5,19 +5,21 @@ from .lie_rotation import LieRotation
 
 
 class SimpleCameraModelInitialGuess(nn.Module):
-
     def __init__(self, num_views: int, num_points: int):
         super().__init__()
         self.focal_length = nn.Parameter(torch.tensor([[0.5]]))
-        self.cx = nn.Parameter(torch.tensor([[0.]]))
-        self.cy = nn.Parameter(torch.tensor([[0.]]))
-        self.orientation = nn.Parameter(torch.tensor([0, 0., 0.]).reshape(1, 1, 1, 1, 3).tile(1, 1, num_views, 1, 1))
+        self.cx = nn.Parameter(torch.tensor([[0.0]]))
+        self.cy = nn.Parameter(torch.tensor([[0.0]]))
+        self.orientation = nn.Parameter(
+            torch.tensor([0, 0.0, 0.0])
+            .reshape(1, 1, 1, 1, 3)
+            .tile(1, 1, num_views, 1, 1)
+        )
         translation = torch.zeros(1, 1, num_views, 3)
         translation[:, :, :, 2] = 10.0
         self.translation = nn.Parameter(translation)
-        world_points = torch.randn(1, 1, num_points, 3)
-        world_points[0, 0, 0, :] = 0.0
-        self.world_points = nn.Parameter(world_points)
+        self.world_xy_points = nn.Parameter(torch.randn(1, 1, num_points - 2, 2))
+        self.world_z_points = nn.Parameter(torch.randn(1, 1, num_points - 3, 1))
 
     def forward(self, projected_points: torch.Tensor) -> SimpleCameraModel:
         batch_size = projected_points.size(0)
@@ -26,7 +28,22 @@ class SimpleCameraModelInitialGuess(nn.Module):
         cy = self.cy.tile(batch_size, 1)
         orientation = self.orientation.tile(batch_size, 1, 1, 1, 1)
         translation = self.translation.tile(batch_size, 1, 1, 1)
-        world_points = self.world_points.tile(batch_size, 1, 1, 1)
+        z_points = torch.cat(
+            [
+                torch.zeros(
+                    1,
+                    1,
+                    1,
+                    1,
+                    device=self.world_z_points.device,
+                    dtype=self.world_z_points.dtype,
+                ),
+                self.world_z_points,
+            ],
+            dim=-2,
+        )
+        world_points = torch.cat([self.world_xy_points, z_points], dim=-1)
+        world_points = world_points.tile(batch_size, 1, 1, 1)
         return SimpleCameraModel(
             focal_length=focal_length,
             cx=cx,
@@ -34,5 +51,5 @@ class SimpleCameraModelInitialGuess(nn.Module):
             orientation=LieRotation(orientation),
             translation=translation,
             world_points=world_points,
-            true_projected_points=projected_points
+            true_projected_points=projected_points,
         )
