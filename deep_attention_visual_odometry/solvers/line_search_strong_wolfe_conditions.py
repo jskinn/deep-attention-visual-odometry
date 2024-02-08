Fix two bugs in the zoom part of the wolfe conditions line search.  - An additional masked update breaking the gradient  - Swapping upper_alpha and lower_alpha in the third conditional, rather than just setting upper=lower.  - Change to using an explicit output value rather than the upper candidate.  - Scale the error and gradient by 1/sqrt(d) before the sum, for numerical stability.

deep_attention_visual_odometry/solvers/line_search_strong_wolfe_conditions.py
@@ -49,11 +49,18 @@ class LineSearchStrongWolfeConditions(nn.Module):
             device=search_direction.device,
         )
 
-        base_error = function.get_error()
-        base_gradient = torch.sum(function.get_gradient() * search_direction, dim=-1)
+        error_scale = torch.tensor(
+            1.0 / function.num_parameters, device=function.device, dtype=torch.float
+        ).sqrt()
+        base_error = error_scale * function.get_error()
+        base_gradient = torch.sum(
+            error_scale * function.get_gradient() * search_direction, dim=-1
+        )
 
         lower_candidate_function = function
         upper_candidate_function = function
+        output_function = function
+        output_step = torch.zeros_like(search_direction)
 
         prev_error = base_error
 
@@ -70,7 +77,7 @@ class LineSearchStrongWolfeConditions(nn.Module):
             upper_candidate_function = upper_candidate_function.masked_update(
                 candidate_function, widening
             )
-            error = upper_candidate_function.get_error()
+            error = error_scale * upper_candidate_function.get_error()
             # First conditional, check if the error has started increasing
             increasing_error = torch.zeros_like(widening)
             increasing_error[widening] = torch.greater(
@@ -89,15 +96,22 @@ class LineSearchStrongWolfeConditions(nn.Module):
             widening = torch.logical_and(widening, torch.logical_not(increasing_error))
             # Second conditional, check if we've actually already met the conditions
             gradient = upper_candidate_function.get_gradient()
-            gradient = (gradient[widening] * search_direction[widening]).sum(dim=-1)
-            met_conditions = torch.less_equal(
+            gradient = (
+                error_scale * gradient[widening] * search_direction[widening]
+            ).sum(dim=-1)
+            met_conditions = torch.zeros_like(widening)
+            met_conditions[widening] = torch.less_equal(
                 gradient.abs(), -self.curvature * base_gradient[widening]
             )
+            output_function = output_function.masked_update(
+                upper_candidate_function, met_conditions
+            )
+            output_step = torch.where(
+                met_conditions[:, :, None], candidate_step, output_step
+            )
             not_met_conditions = torch.logical_not(met_conditions)
-            still_widening = torch.zeros_like(widening)
-            gradient = gradient[not_met_conditions]
-            still_widening[widening] = not_met_conditions
-            widening = torch.logical_and(widening, still_widening)
+            gradient = gradient[not_met_conditions[widening]]
+            widening = torch.logical_and(widening, not_met_conditions)
             # Third conditional, positive gradient
             positive_gradient = torch.greater_equal(gradient, 0.0)
             swapping = torch.zeros_like(widening)
@@ -127,21 +141,23 @@ class LineSearchStrongWolfeConditions(nn.Module):
             prev_error = error
 
         # Algorithm 3.6: Zoom on bounded ranges to find a point that satisfies the conditions
-        lower_error = lower_candidate_function.get_error()
         for idx in range(self.zoom_iterations):
             # Linearly interpolate the high and low gradients to pick a new alpha
             lower_gradient = lower_candidate_function.get_gradient()
             upper_gradient = upper_candidate_function.get_gradient()
-            lower_gradient = (lower_gradient[zooming] * search_direction[zooming]).sum(
-                dim=-1
-            )
-            upper_gradient = (upper_gradient[zooming] * search_direction[zooming]).sum(
-                dim=-1
-            )
+            lower_gradient = (
+                error_scale * lower_gradient[zooming] * search_direction[zooming]
+            ).sum(dim=-1)
+            upper_gradient = (
+                error_scale * upper_gradient[zooming] * search_direction[zooming]
+            ).sum(dim=-1)
             gradient_diff = upper_gradient - lower_gradient
-            candidate_alpha = lower_alpha[zooming] - lower_gradient * (
-                upper_alpha[zooming] - lower_alpha[zooming]
-            ) / (gradient_diff + 1e-8)
+            candidate_alpha = (
+                lower_alpha[zooming]
+                - lower_gradient
+                * (upper_alpha[zooming] - lower_alpha[zooming])
+                / gradient_diff
+            )
             # Fall back to bisection if the gradients were the same, or the candidate is outside our range
             non_linear_alpha = torch.logical_or(
                 torch.eq(gradient_diff, 0.0),
@@ -168,11 +184,10 @@ class LineSearchStrongWolfeConditions(nn.Module):
                 candidate_alpha[:, None] * search_direction[zooming]
             )
             candidate_function = function.add(candidate_step)
-            candidate_function = upper_candidate_function.masked_update(
-                candidate_function, zooming
-            )
+
             # Evaluate the error
-            error = candidate_function.get_error()
+            lower_error = error_scale * lower_candidate_function.get_error()
+            error = error_scale * candidate_function.get_error()
             # Conditional one: Has the error at this intermediate increased from the low point
             #                  or a linear interpolation from the base?
             increasing_error = torch.zeros_like(zooming)
@@ -201,20 +216,24 @@ class LineSearchStrongWolfeConditions(nn.Module):
             # Compute the gradient
             gradient = candidate_function.get_gradient()
             gradient = (
-                gradient[non_increasing_error] * search_direction[non_increasing_error]
+                error_scale
+                * gradient[non_increasing_error]
+                * search_direction[non_increasing_error]
             ).sum(dim=-1)
             # Conditional 2: Does the candidate meet the conditions
             met_conditions = torch.zeros_like(non_increasing_error)
             met_conditions[non_increasing_error] = torch.less_equal(
                 gradient.abs(), -self.curvature * base_gradient[non_increasing_error]
             )
-            # Where true, a_upper = a_j and stop
-            upper_alpha = torch.where(met_conditions, candidate_alpha_wide, upper_alpha)
-            upper_candidate_function = upper_candidate_function.masked_update(
+            # Where true, a* = a_j and stop
+            output_function = output_function.masked_update(
                 candidate_function, met_conditions
             )
+            output_step = torch.where(
+                met_conditions[:, :, None], candidate_step, output_step
+            )
             zooming = torch.logical_and(zooming, torch.logical_not(met_conditions))
-            # Conditional 3: Swap low and high if the gradient points the other way
+            # Conditional 3: Set high to low if the gradient becomes positive
             reversed_gradient = torch.greater_equal(
                 gradient
                 * (
@@ -227,18 +246,10 @@ class LineSearchStrongWolfeConditions(nn.Module):
             swapping[non_increasing_error] = torch.logical_and(
                 reversed_gradient, zooming[non_increasing_error]
             )
-            new_upper_alpha = torch.where(swapping, lower_alpha, upper_alpha)
-            new_lower_alpha = torch.where(swapping, upper_alpha, lower_alpha)
-            upper_alpha = new_upper_alpha
-            lower_alpha = new_lower_alpha
-            new_lower_candidate_function = lower_candidate_function.masked_update(
-                upper_candidate_function, swapping
-            )
-            new_upper_candidate_function = upper_candidate_function.masked_update(
+            upper_alpha = torch.where(swapping, lower_alpha, upper_alpha)
+            upper_candidate_function = upper_candidate_function.masked_update(
                 lower_candidate_function, swapping
             )
-            lower_candidate_function = new_lower_candidate_function
-            upper_candidate_function = new_upper_candidate_function
             # Update the lower alpha for the else case
             non_increasing_error = torch.logical_and(non_increasing_error, zooming)
             lower_alpha = torch.where(
@@ -248,4 +259,13 @@ class LineSearchStrongWolfeConditions(nn.Module):
                 candidate_function, non_increasing_error
             )
 
-        return upper_candidate_function, upper_alpha[:, :, None] * search_direction
+        # If the zooming didn't finish, we want to pick a step anyway
+        output_function = output_function.masked_update(
+            lower_candidate_function, zooming
+        )
+        output_step = torch.where(
+            zooming[:, :, None],
+            lower_alpha[:, None] * search_direction,
+            output_step,
+        )
+        return output_function, output_step