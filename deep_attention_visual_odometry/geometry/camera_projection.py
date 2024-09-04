eb4d7377d65ecf9382fd0049eb7b6ecf19492561
John Skinner <jskinn7@gmail.com>
2024-09-04 16:55:55 +1000

    Replace the basic pinhole camera projection with a single function, since we can use autograd to differentiate.

--- deep_attention_visual_odometry/geometry/camera_projection.py
+++ deep_attention_visual_odometry/geometry/camera_projection.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2024  John Skinner
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+# USA
 from typing import Self
 import torch
 
@@ -32,8 +48,6 @@ class PinholeCameraProjection:
         """
         z = points[..., 2:3]
         f_on_z = self.focal_length / z
-        
-
 
     def vector_gradient(self) -> torch.Tensor:
         """
@@ -49,3 +63,20 @@ class PinholeCameraProjection:
     def masked_update(self, other: Self, mask: torch.Tensor) -> Self:
         new_parameters = torch.where(mask, other._parameters, self._parameters)
         return type(self)(new_parameters)
+
+
+def project_points_basic_pinhole(
+    points: torch.Tensor, intrinsics: torch.Tensor
+) -> torch.Tensor:
+    """
+    Basic pinhole camera projection, of 3D points using three intrinsics: f, cx, and cy.
+    This function assumes the final dimension
+    :param points: A (Bx)
+    :param intrinsics:
+    :return:
+    """
+    focal_length = intrinsics[..., 0:1]
+    principal_point = intrinsics[..., 1:3]
+    xy = points[..., 0:2]
+    z = points[..., 2:3]
+    return focal_length * xy / z + principal_point
--- tests/geometry/test_camera_projection.py
+++ tests/geometry/test_camera_projection.py
@@ -1,5 +1,25 @@
+# Copyright (C) 2024  John Skinner
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+# USA
 import torch
-from deep_attention_visual_odometry.geometry.camera_projection import PinholeCameraProjection
+
+from deep_attention_visual_odometry.autograd_solvers import SGDSolver
+from deep_attention_visual_odometry.geometry.camera_projection import (
+    project_points_basic_pinhole,
+)
 
 
 def test_project_single_point():
@@ -13,41 +33,72 @@ def test_project_single_point():
     v = f * y / z + cy
     camera_intrinsics = torch.tensor([f, cx, cy], dtype=torch.float64)
     point = torch.tensor([x, y, z], dtype=torch.float64)
-    projector = PinholeCameraProjection(camera_intrinsics)
-    pixel = projector.project_points(point)
+    pixel = project_points_basic_pinhole(point, camera_intrinsics)
     assert pixel.shape == (2,)
     assert pixel[0] == u
     assert pixel[1] == v
 
 
 def test_project_batch_of_points_and_views():
-    camera_intrinsics = torch.tensor([
-        [0.899, 0.1, -0.15],
-        [1.0101, -0.1, 0.08],
-    ], dtype=torch.float64).unsqueeze(1)
-    points = torch.tensor([
-        [1.0, 1.0, 14.0],
-        [1.0, -1.0, 14.0],
-        [-1.0, 1.0, 14.0],
-        [-1.0, -1.0, 14.0],
-    ], dtype=torch.float64).unsqueeze(0)
-    expected_pixels = torch.tensor([
+    camera_intrinsics = torch.tensor(
         [
-            [0.899 * 1.0 / 14.0 + 0.1, 0.899 * 1.0 / 14.0 - 0.15],
-            [0.899 * 1.0 / 14.0 + 0.1, 0.899 * -1.0 / 14.0 - 0.15],
-            [0.899 * -1.0 / 14.0 + 0.1, 0.899 * 1.0 / 14.0 - 0.15],
-            [0.899 * -1.0 / 14.0 + 0.1, 0.899 * -1.0 / 14.0 - 0.15],
-        ],[
-            [1.0101 * 1.0 / 14.0 - 0.1, 1.0101 * 1.0 / 14.0 + 0.08],
-            [1.0101 * 1.0 / 14.0 - 0.1, 1.0101 * -1.0 / 14.0 + 0.08],
-            [1.0101 * -1.0 / 14.0 - 0.1, 1.0101 * 1.0 / 14.0 + 0.08],
-            [1.0101 * -1.0 / 14.0 - 0.1, 1.0101 * -1.0 / 14.0 + 0.08],
-        ]
-    ], dtype=torch.float64)
-    projector = PinholeCameraProjection(camera_intrinsics)
-    pixels = projector.project_points(points)
+            [0.899, 0.1, -0.15],
+            [1.0101, -0.1, 0.08],
+        ],
+        dtype=torch.float64,
+    ).unsqueeze(1)
+    points = torch.tensor(
+        [
+            [1.0, 1.0, 14.0],
+            [1.0, -1.0, 14.0],
+            [-1.0, 1.0, 14.0],
+            [-1.0, -1.0, 14.0],
+        ],
+        dtype=torch.float64,
+    ).unsqueeze(0)
+    expected_pixels = torch.tensor(
+        [
+            [
+                [0.899 * 1.0 / 14.0 + 0.1, 0.899 * 1.0 / 14.0 - 0.15],
+                [0.899 * 1.0 / 14.0 + 0.1, 0.899 * -1.0 / 14.0 - 0.15],
+                [0.899 * -1.0 / 14.0 + 0.1, 0.899 * 1.0 / 14.0 - 0.15],
+                [0.899 * -1.0 / 14.0 + 0.1, 0.899 * -1.0 / 14.0 - 0.15],
+            ],
+            [
+                [1.0101 * 1.0 / 14.0 - 0.1, 1.0101 * 1.0 / 14.0 + 0.08],
+                [1.0101 * 1.0 / 14.0 - 0.1, 1.0101 * -1.0 / 14.0 + 0.08],
+                [1.0101 * -1.0 / 14.0 - 0.1, 1.0101 * 1.0 / 14.0 + 0.08],
+                [1.0101 * -1.0 / 14.0 - 0.1, 1.0101 * -1.0 / 14.0 + 0.08],
+            ],
+        ],
+        dtype=torch.float64,
+    )
+    pixels = project_points_basic_pinhole(points, camera_intrinsics)
     assert torch.equal(pixels, expected_pixels)
 
 
+def test_intrinsics_can_be_optimised():
+    points = torch.tensor(
+        [
+            [1.0, 1.0, 14.0],
+            [1.0, -1.0, 14.0],
+            [-1.0, 1.0, 14.0],
+            [-1.0, -1.0, 14.0],
+        ],
+        dtype=torch.float64,
+    )
+    true_intrinsics = torch.tensor([[0.899, -0.15, 0.08]], dtype=torch.float64)
+    intrinsics = torch.tensor([[0.989, -0.1, 0.15]], dtype=torch.float64)
+    true_projection = project_points_basic_pinhole(points, true_intrinsics)
+    solver = SGDSolver(learning_rate=0.0002, iterations=2000)
 
+    def error_function(x: torch.Tensor) -> torch.Tensor:
+        projected_points = project_points_basic_pinhole(
+            points[None, :, :], x[:, None, :]
+        )
+        return torch.linalg.vector_norm(
+            projected_points - true_projection[None, :, :], dim=-1
+        ).sum(dim=-1)
 
+    result = solver(intrinsics, error_function)
+    assert torch.isclose(result, true_intrinsics, atol=6e-5, rtol=1e-4).all()
