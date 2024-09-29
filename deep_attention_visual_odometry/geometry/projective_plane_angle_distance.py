56aa0ed15938582c08d4b20ebb56b759b98e06ad
John Skinner <jskinn7@gmail.com>
2024-09-29 13:26:55 +1000

    Change to a numerically stable calculation of the angle between vectors. Add many more unit tests for it.

--- deep_attention_visual_odometry/geometry/projective_plane_angle_distance.py
+++ deep_attention_visual_odometry/geometry/projective_plane_angle_distance.py
@@ -26,17 +26,39 @@ def projective_plane_angle_distance(
     The distance between two sets of homogeneous coordinates, defined as the angle between them in R^3.
     Ranges from 0 to 2pi.
 
-    This will have divide-by-zero errors if given (0:0:0), which is an invalid homogeneous coordinate.
+    Uses Kahan's formulation from "Miscalculating area and angles of a Needle-Like Triangle",
+    which is more numerically stable than the arccos or arctan approaches.
+
+    This will produce NaN if given (0:0:0), which is an invalid homogeneous coordinate.
 
     :param projective_points_a: A (B...)x3 vector of homogeneous-coordinates
     :param projective_points_b: A (B...)x3 vector of homogeneous-coordinates
+    :param keepdim: Whether to retain the final dimension as size 1.
     :return: A (B...) or (B...)x1 tensor of the distances between each pair of coordinates
     """
+    # Calculate angle as 2 arctan2(|v1 / |v1| - v2 / |v2||, |v1 / |v1| + v2 / |v2||
+    # Comes from W. Kahan "Computing Cross-Products and Rotations in 2- and 3-Dimensional Euclidean Spaces".
+    # See https://scicomp.stackexchange.com/questions/27689/numerically-stable-way-of-computing-angles-between-vectors
+    # Consider the parallelogram formed by adding the two vectors in different orders.
+    # The angle we are interested in is one of the internal angles of the parallelogram.
+    # Normalizing the two vectors doesn't change the angle and addresses issues with extremely large
+    # and extremely small vectors.
+    # One diagonal of the parallelogram is the sum of the two vectors, the other is the difference.
+    # The two diagonals form a right-angled triangle, with side lengths half the diagonals and internal
+    # angle half the angle between the vectors.
+    # tan(theta / 2) = (|v1 - v2| / 2) / (|v1 + v2| / 2)
+    # Rearranging gives the above equation.
     projective_points_a = projective_points_a / torch.linalg.vector_norm(
         projective_points_a, dim=-1, keepdim=True
     )
     projective_points_b = projective_points_b / torch.linalg.vector_norm(
         projective_points_b, dim=-1, keepdim=True
     )
-    cosine = (projective_points_a * projective_points_b).sum(dim=-1, keepdims=keepdim)
-    return torch.acos(cosine.clamp(min=-1.0, max=1.0))
+    unit_vector_sum = torch.linalg.vector_norm(
+        projective_points_a + projective_points_b, dim=-1, keepdim=keepdim
+    )
+    unit_vector_diff = torch.linalg.vector_norm(
+        projective_points_a - projective_points_b, dim=-1, keepdim=keepdim
+    )
+    result = 2.0 * torch.atan2(unit_vector_diff, unit_vector_sum)
+    return result
--- tests/geometry/test_projective_plane_angle_distance.py
+++ tests/geometry/test_projective_plane_angle_distance.py
@@ -20,40 +20,34 @@ import pytest
 import numpy as np
 import torch
 from torch.nn import Module
+import torch.autograd
 
 from deep_attention_visual_odometry.geometry.projective_plane_angle_distance import (
     projective_plane_angle_distance,
 )
 
 
-def test_returns_angle_between_vectors() -> None:
-    reference_vector = torch.tensor([0.0, 0.0, 1.0])
-
-    angle = 7 * math.pi / 16
-    vector = torch.tensor([math.sin(angle), 0.0, math.cos(angle)])
+@pytest.mark.parametrize("angle", [math.pi * idx / 36 for idx in range(37)])
+def test_returns_angle_between_unit_vectors(angle: float) -> None:
+    reference_vector = torch.tensor([0.0, 0.0, 1.0], dtype=torch.float32)
+    vector = torch.tensor([math.sin(angle), 0.0, math.cos(angle)], dtype=torch.float32)
     result = projective_plane_angle_distance(reference_vector, vector)
-    assert result == pytest.approx(angle, rel=0, abs=1e-7)
-
-    angle = 11 * math.pi / 12
-    vector = torch.tensor([0.0, math.sin(angle), math.cos(angle)])
-    result = projective_plane_angle_distance(reference_vector, vector)
-    assert result == pytest.approx(angle, rel=0, abs=1e-7)
+    assert float(result) == pytest.approx(angle, rel=0.0, abs=1.5e-6)
 
 
-def test_normalizes_vectors() -> None:
-    reference_vector = torch.tensor([0.0, 0.0, 2.233])
-
-    angle = 5 * math.pi / 12
-    length = 5.325
-    vector = torch.tensor([length * math.sin(angle), 0.0, length * math.cos(angle)])
-    result = projective_plane_angle_distance(reference_vector, vector)
-    assert result == pytest.approx(angle, rel=0, abs=1e-7)
-
-    angle = 4 * math.pi / 15
-    length = 0.0214
-    vector = torch.tensor([0.0, length * math.sin(angle), length * math.cos(angle)])
-    result = projective_plane_angle_distance(reference_vector, vector)
-    assert result == pytest.approx(angle, rel=0, abs=1e-7)
+@pytest.mark.parametrize("angle", [math.pi * idx / 39 for idx in range(40)])
+def test_returns_angle_between_vectors_of_arbitrary_length(
+    fixed_random_seed: int, angle: float
+) -> None:
+    rng = np.random.default_rng(fixed_random_seed)
+    lengths = rng.uniform(0.0, 10000.0, size=2)
+    vector_1 = torch.tensor([0.0, lengths[0], 0.0], dtype=torch.float32)
+    vector_2 = torch.tensor(
+        [0.0, lengths[1] * math.cos(angle), lengths[1] * math.sin(angle)],
+        dtype=torch.float32,
+    )
+    result = projective_plane_angle_distance(vector_1, vector_2)
+    assert float(result) == pytest.approx(angle, rel=0.0, abs=5e-6)
 
 
 def test_returns_nan_if_given_zeros() -> None:
@@ -65,6 +59,13 @@ def test_returns_nan_if_given_zeros() -> None:
     assert torch.isnan(result).all()
     result = projective_plane_angle_distance(zeros, zeros)
     assert torch.isnan(result).all()
+
+
+def test_returns_zero_if_given_parallel_vectors() -> None:
+    vector_1 = torch.tensor([3.0, 0.5, -0.4096])
+    vector_2 = 2.125 * vector_1
+    result = projective_plane_angle_distance(vector_1, vector_2)
+    assert result == 0.0  # pytest.approx(0.0, rel=0, abs=1e-7)
 
 
 def test_supports_batch_and_broadcasts(fixed_random_seed: int) -> None:
@@ -96,9 +97,9 @@ def test_angle_between_vectors_is_zero_for_identical_vectors(
     fixed_random_seed: int,
 ) -> None:
     rng = np.random.default_rng(fixed_random_seed)
-    vectors = torch.tensor(rng.normal(size=(7, 3)))
+    vectors = torch.tensor(rng.normal(size=(7, 3)), dtype=torch.float64)
     result = projective_plane_angle_distance(vectors, vectors)
-    assert torch.isclose(result, torch.zeros_like(result), rtol=0.0, atol=1e-7).all()
+    assert torch.equal(result, torch.zeros_like(result))
 
 
 def test_angle_between_vectors_is_pi_for_negative_vectors(
@@ -110,6 +111,196 @@ def test_angle_between_vectors_is_pi_for_negative_vectors(
     assert torch.isclose(
         result, math.pi * torch.ones_like(result), rtol=0.0, atol=1e-7
     ).all()
+
+
+def test_supports_needle_like_triangles():
+    # Some example "needle-like" triangles, based on
+    # "Miscalculating the Area and Angles of Needle-like triangles" by Kahan
+    scale = 100000.0
+    angle = 1.145915591e-7
+    vector_1 = torch.tensor([scale, 0.0, 0.0], dtype=torch.float32)
+    vector_2 = torch.tensor(
+        [scale * math.cos(angle), 0.0, scale * math.sin(angle)], dtype=torch.float32
+    )
+    result = projective_plane_angle_distance(vector_1, vector_2)
+    torch.equal(result, torch.tensor(angle, dtype=torch.float32))
+
+    angle = 48.18968510
+    vector_1 = torch.tensor([0.0, 0.0, 100000.0], dtype=torch.float32)
+    vector_2 = torch.tensor(
+        [0.0, 0.0001 * math.sin(angle), 0.0001 * math.cos(angle)], dtype=torch.float32
+    )
+    result = projective_plane_angle_distance(vector_1, vector_2)
+    torch.equal(result, torch.tensor(angle, dtype=torch.float32))
+
+    angle = math.radians(179.9985965)
+    vector_1 = torch.tensor([0.0, 10000.0, 0.0], dtype=torch.float32)
+    vector_2 = torch.tensor(
+        [5000 * math.sin(angle), 5000 * math.cos(angle), 0.0], dtype=torch.float32
+    )
+    result = projective_plane_angle_distance(vector_1, vector_2)
+    torch.equal(result, torch.tensor(angle, dtype=torch.float32))
+
+
+@pytest.mark.parametrize("axis", [0, 1, 2])
+@pytest.mark.parametrize("change_direction", [10000.0, -10000.0])
+def test_is_relatively_stable_for_small_changes(
+    axis: int, change_direction: float
+) -> None:
+    vector_values = (-0.84278762, 3.2473695, -1.18345099)
+    base_vector = torch.tensor(vector_values, dtype=torch.float64)
+
+    def step_vector(values: tuple[float, float, float]) -> tuple[float, float, float]:
+        if axis == 0:
+            return np.nextafter(values[0], change_direction), values[1], values[2]
+        elif axis == 1:
+            return values[0], np.nextafter(values[1], change_direction), values[2]
+        return values[0], values[1], np.nextafter(values[2], change_direction)
+
+    results = []
+    for step_idx in range(100):
+        vector_values = step_vector(vector_values)
+        vector = torch.tensor(vector_values, dtype=torch.float64)
+        result = projective_plane_angle_distance(base_vector, vector)
+        results.append(float(result))
+    assert (
+        sum(result_2 >= result_1 for result_1, result_2 in zip(results, results[1:]))
+        > 90
+    )
+    assert len(set(results)) > 92
+
+
+@pytest.mark.parametrize("angle", [math.pi * idx / 38 for idx in range(39)])
+def test_can_pass_through_gradients(fixed_random_seed: int, angle: float) -> None:
+    rng = np.random.default_rng(fixed_random_seed)
+    lengths = rng.uniform(0.0, 10000.0, size=2)
+    vector_1 = torch.tensor(
+        [lengths[0], 0.0, 0.0], dtype=torch.float32, requires_grad=True
+    )
+    vector_2 = torch.tensor(
+        [lengths[1] * math.cos(angle), 0.0, lengths[1] * math.sin(angle)],
+        dtype=torch.float32,
+        requires_grad=True,
+    )
+    distance = projective_plane_angle_distance(vector_1, vector_2)
+    gradients = torch.autograd.grad(distance, (vector_1, vector_2))
+    assert len(gradients) == 2
+    assert torch.isfinite(gradients[0]).all()
+    if angle > 0:
+        assert not torch.eq(gradients[0], torch.zeros_like(gradients[0])).all()
+    assert torch.isfinite(gradients[1]).all()
+    if angle > 0:
+        assert not torch.eq(gradients[1], torch.zeros_like(gradients[1])).all()
+
+
+def test_gradients_at_parallel_are_zero(fixed_random_seed: int) -> None:
+    rng = np.random.default_rng(fixed_random_seed)
+    vectors = torch.tensor(rng.normal(size=3), dtype=torch.float64, requires_grad=True)
+    distance = projective_plane_angle_distance(vectors, vectors.detach())
+    gradient = torch.autograd.grad(distance, vectors)
+    assert torch.isfinite(gradient[0]).all()
+    assert torch.equal(gradient[0], torch.zeros_like(gradient[0]))
+
+    distance = projective_plane_angle_distance(vectors.detach(), vectors)
+    gradient = torch.autograd.grad(distance, vectors)
+    assert torch.isfinite(gradient[0]).all()
+    assert torch.equal(gradient[0], torch.zeros_like(gradient[0]))
+
+
+def test_gradients_are_valid_near_parallel() -> None:
+    vector_1 = torch.tensor(
+        [0.16747557919176526, 1.411436462001734, 17.299996837614568],
+        dtype=torch.float64,
+        requires_grad=True,
+    )
+    vector_2 = torch.tensor(
+        [0.1674757565885585, 1.411436913882588, 17.3], dtype=torch.float64
+    )
+    distance = projective_plane_angle_distance(vector_1, vector_2)
+    gradient = torch.autograd.grad(distance, vector_1)
+    assert torch.isfinite(gradient[0]).all()
+
+    vector_1 = torch.tensor(
+        [1.2658736503683827, 1.411436462001734, 17.299996837614568], dtype=torch.float64
+    )
+    vector_2 = torch.tensor(
+        [1.265873827765176, 1.411436913882588, 17.3],
+        dtype=torch.float64,
+        requires_grad=True,
+    )
+    distance = projective_plane_angle_distance(vector_1, vector_2)
+    gradient = torch.autograd.grad(distance, vector_2)
+    assert torch.isfinite(gradient[0]).all()
+
+
+def test_gradients_are_valid_for_right_angles(fixed_random_seed: int) -> None:
+    rng = np.random.default_rng(fixed_random_seed)
+    vectors_1 = torch.tensor(
+        rng.normal(size=(7, 3)), dtype=torch.float64, requires_grad=True
+    )
+    vectors_2 = torch.tensor(rng.normal(size=(7, 3)), dtype=torch.float64)
+    unit_1 = vectors_1.detach() / torch.linalg.vector_norm(
+        vectors_1.detach(), dim=-1, keepdim=True
+    )
+    vectors_2 = vectors_2 - unit_1 * (unit_1 * vectors_2).sum(dim=-1, keepdims=True)
+
+    distance = projective_plane_angle_distance(vectors_1, vectors_2)
+    gradient = torch.autograd.grad(distance.sum(), vectors_1)
+    assert torch.isfinite(gradient[0]).all()
+    assert torch.greater(gradient[0].abs(), torch.zeros_like(gradient[0])).all()
+
+    distance = projective_plane_angle_distance(vectors_2, vectors_1)
+    gradient = torch.autograd.grad(distance.sum(), vectors_1)
+    assert torch.isfinite(gradient[0]).all()
+    assert torch.greater(gradient[0].abs(), torch.zeros_like(gradient[0])).all()
+
+
+def test_gradients_are_zero_for_negative_vectors(fixed_random_seed: int) -> None:
+    rng = np.random.default_rng(fixed_random_seed)
+    vectors = torch.tensor(
+        rng.normal(size=(7, 3)), dtype=torch.float64, requires_grad=True
+    )
+    distance = projective_plane_angle_distance(vectors, -1.0 * vectors.detach())
+    gradient = torch.autograd.grad(distance.sum(), vectors)
+    assert torch.isfinite(gradient[0]).all()
+    assert torch.isclose(gradient[0], torch.zeros_like(gradient[0])).all()
+
+    distance = projective_plane_angle_distance(-1.0 * vectors.detach(), vectors)
+    gradient = torch.autograd.grad(distance.sum(), vectors)
+    assert torch.isfinite(gradient[0]).all()
+    assert torch.isclose(gradient[0], torch.zeros_like(gradient[0])).all()
+
+
+@pytest.mark.parametrize(
+    "dtype,eps, atol, rtol",
+    [
+        (
+            torch.float64,
+            1e-6,
+            1e-05,
+            0.001,
+        ),
+        (
+            torch.float32,
+            1e-3,
+            1e-04,
+            0.005,
+        ),
+    ],
+)
+def test_gradients_gradcheck(
+    fixed_random_seed: int, dtype: torch.dtype, eps: float, atol: float, rtol: float
+) -> None:
+    rng = np.random.default_rng(fixed_random_seed)
+    vectors_1 = torch.tensor(rng.normal(size=(48, 3)), dtype=dtype, requires_grad=True)
+    vectors_2 = torch.tensor(rng.normal(size=(48, 3)), dtype=dtype, requires_grad=True)
+    assert torch.autograd.gradcheck(
+        projective_plane_angle_distance,
+        (vectors_1, vectors_2),
+        eps=eps,
+        atol=atol,
+        rtol=rtol,
+    )
 
 
 class CompileModule(Module):
