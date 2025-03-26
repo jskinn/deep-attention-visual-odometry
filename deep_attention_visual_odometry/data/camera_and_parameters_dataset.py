b2cdcadbe31e6b31fa6d17966ab985e01da272ed
John Skinner <jskinn7@gmail.com>
2025-03-26 14:19:21 +1000

    Rewrite the camera and parameters dataset to use the same parameterisation as the updated camera model.

--- deep_attention_visual_odometry/data/camera_and_parameters_dataset.py
+++ deep_attention_visual_odometry/data/camera_and_parameters_dataset.py
@@ -1,6 +1,29 @@
+# Copyright (C) 2024  John Skinner
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+# USA
+from transforms3d.axangles import mat2axangle
 import torch
 from torch.utils.data import Dataset
+
 from deep_attention_visual_odometry.base_types import CameraViewsAndPoints
+from deep_attention_visual_odometry.camera_model import get_camera_relative_points
+from deep_attention_visual_odometry.geometry import (
+    rotate_vector_axis_angle,
+project_points_pinhole_homogeneous
+)
 
 
 class CameraAndParametersDataset(Dataset[CameraViewsAndPoints]):
@@ -14,24 +37,26 @@ class CameraAndParametersDataset(Dataset[CameraViewsAndPoints]):
         num_points: int,
         num_views: int,
         min_camera_distance: float = 0.1,
+        dtype: torch.dtype = torch.float32,
     ):
         self.epoch_length = int(epoch_length)
         self._num_points = int(num_points)
         self._num_views = int(num_views)
         self._min_camera_distance = float(min_camera_distance)
+        self._dtype = dtype
 
         self._camera_distance_mean = 20.0
         self._camera_distance_std = 5.0
         self._camera_location_spread = torch.tensor(
-            [3.0, 3.0, 3.0], dtype=torch.float32
+            [3.0, 3.0, 3.0], dtype=self._dtype
         )
-        self._camera_target_spread = torch.tensor([3.0, 3.0, 3.0], dtype=torch.float32)
-        self._camera_up_spread = torch.tensor([3.0, 3.0, 3.0], dtype=torch.float32)
+        self._camera_target_spread = torch.tensor([3.0, 3.0, 3.0], dtype=self._dtype)
+        self._camera_up_spread = torch.tensor([3.0, 3.0, 3.0], dtype=self._dtype)
         self._transform_std = torch.tensor(
             [5.0, 5.0, 5.0, torch.pi / 3, torch.pi / 3, torch.pi / 3],
-            dtype=torch.float32,
+            dtype=self._dtype,
         )
-        self._points_std = torch.tensor([3.0, 3.0, 3.0], dtype=torch.float32).reshape(
+        self._points_std = torch.tensor([3.0, 3.0, 3.0], dtype=self._dtype).reshape(
             1, 3
         )
 
@@ -43,90 +68,67 @@ class CameraAndParametersDataset(Dataset[CameraViewsAndPoints]):
             raise IndexError(f"Index {item} out of range")
 
         world_points = self._generate_world_points()
-        camera_extrinsics = self._generate_camera_extrinisics(world_points)
+        camera_translations, camera_rotations = self._generate_camera_extrinisics(world_points)
         camera_intrinsics = self._generate_camera_intrinsics()
         projected_points, visibility_mask = self._project_points(
-            world_points, camera_extrinsics, camera_intrinsics
+            world_points, camera_translations, camera_rotations, camera_intrinsics
         )
         return CameraViewsAndPoints(
             world_points=world_points,
-            camera_extrinsics=camera_extrinsics,
             camera_intrinsics=camera_intrinsics,
+            camera_orientations=camera_rotations,
+            camera_translations=camera_translations,
             projected_points=projected_points,
             visibility_mask=visibility_mask,
         )
 
     def _generate_world_points(self) -> torch.Tensor:
-        world_points = self._points_std * torch.randn(
-            self._num_points - 2, 3, dtype=torch.float32
+        """
+        Generate world points relative to the first view
+        """
+        # All points should be in front of the first camera, so Z should be positive.
+        z_points = self._camera_distance_mean + self._camera_distance_std * torch.randn(self._num_points, 1, dtype=self._dtype)
+        xy_points = self._points_std * torch.randn(
+            self._num_points, 2, dtype=self._dtype
         )
-        # Setting zeros in the first 3 points constrains the origin and orientation of the points
-        world_points[0, 2] = 0.0
-        return torch.cat(
-            [
-                torch.tensor(
-                    [
-                        [0.0, 0.0, 0.0],
-                        [1.0, 0.0, 0.0],
-                    ],
-                    dtype=torch.float32,
-                ),
-                world_points,
-            ],
-            dim=0,
-        )
+        return torch.concatenate([xy_points, z_points.abs()], dim=-1)
 
-    def _generate_camera_extrinisics(self, world_points: torch.Tensor) -> torch.Tensor:
+    def _generate_camera_extrinisics(self, world_points: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         # - Pick three points: A location, a view target, and an up direction
         # - Perturb each of those base points for each camera
         # - Orthonomralise (target - location) and (up - location) to produce a rotation matrix
         # This should give us n views with similar but spread out locations,
         # looking approximately through the centre of mass of the points
-        world_centre = world_points.mean(dim=0)
-        # camera_direction = torch.randn(3, dtype=torch.float32)
-        # camera_direction = camera_direction / torch.linalg.vector_norm(
-        #     camera_direction, dim=-1, keepdim=True
-        # )
-        camera_direction = torch.tensor([0.0, 0.0, -1.0], dtype=torch.float32)
-        # up_direction = torch.randn(3, dtype=torch.float32)
-        # up_direction = up_direction / torch.linalg.vector_norm(
-        #     up_direction, dim=-1, keepdim=True
-        # )
-        up_direction = torch.tensor([0.0, -1.0, 0.0], dtype=torch.float32)
-        camera_distance = (
-            self._camera_distance_mean
-            + self._camera_distance_std * torch.randn(1, dtype=torch.float32)
-        ).abs()
-        view_distance = (
-            self._camera_distance_mean
-            + self._camera_distance_std * torch.randn(1, dtype=torch.float32)
-        ).abs()
         up_distance = (
             self._camera_distance_mean
-            + self._camera_distance_std * torch.randn(1, dtype=torch.float32)
+            + self._camera_distance_std * torch.randn(1, dtype=self._dtype)
         ).abs()
-        camera_location_base = world_centre + camera_distance * camera_direction
-        camera_target_base = world_centre - view_distance * camera_direction
-        up_base = camera_location_base + up_distance * up_direction
+        camera_target_base = world_points.mean(dim=0) * (1.0 + torch.rand()) + (self._points_std / 2.0) * torch.randn(3, dtype=self._dtype)
+        up_base = up_distance * torch.tensor([0.0, -1.0, 0.0], dtype=self._dtype)
 
         # Generate different reference points for each camera
-        camera_locations = camera_location_base[None, :] + self._camera_location_spread[
-            None, :
-        ] * torch.randn(self._num_views, 3, dtype=torch.float32)
+        camera_locations = self._camera_location_spread[None, :] * torch.randn(self._num_views, 3, dtype=self._dtype)
         camera_targets = camera_target_base[None, :] + self._camera_target_spread[
             None, :
-        ] * torch.randn(self._num_views, 3, dtype=torch.float32)
+        ] * torch.randn(self._num_views, 3, dtype=self._dtype)
         camera_up = up_base[None, :] + self._camera_up_spread[None, :] * torch.randn(
-            self._num_views, 3, dtype=torch.float32
+            self._num_views, 3, dtype=self._dtype
         )
 
         # Orthonormalise to produce a rotation matrix
         forward = camera_targets - camera_locations
         up = camera_up - camera_locations
         forward = forward / torch.linalg.vector_norm(forward, dim=-1, keepdim=True)
-        up = up - forward * (forward * up).sum(dim=-1, keepdims=True)
+        up = up - forward * (forward * up).sum(dim=-1, keepdim=True)
         up = up / torch.linalg.vector_norm(up, dim=-1, keepdim=True)
         left = torch.linalg.cross(forward, up)
+
+        # Make rotation matrices in numpy, and convert them to axis-angle form.
+        rotation_matrix = torch.stack([-left, -up, forward], dim=-2).numpy()
+        axis, angle = mat2axangle(rotation_matrix)
+        angle = torch.tensor(angle)
+        axis = torch.tensor(axis)
+        camera_rotations = angle * axis / torch.linalg.vector_norm(axis, dim=-1, keepdim=True)
 
         # Check that the points are all at least the minimum distance away
         z_distances = (
@@ -140,18 +142,7 @@ class CameraAndParametersDataset(Dataset[CameraViewsAndPoints]):
         )
         camera_locations = camera_locations - z_distances[:, None] * forward
 
-        # Assemble 4x4 extrinisics matrices
-        transforms = torch.zeros(self._num_views, 3, 4, dtype=torch.float32)
-        transforms[:, 0:3, 0] = -left
-        transforms[:, 0:3, 1] = -up
-        transforms[:, 0:3, 2] = forward
-        transforms[:, 0:3, 3] = camera_locations
-
-        # TODO: This should be easier than linalg.inv,
-        #       since we know that R^-1 = R^T
-        # Assemble inverse matrices
-
-        return transforms
+        return camera_locations, camera_rotations
 
     def _generate_camera_intrinsics(self) -> torch.Tensor:
         angle = 3 * torch.pi / 18 + (9 * torch.pi / 18) * torch.rand(1)
@@ -162,9 +153,22 @@ class CameraAndParametersDataset(Dataset[CameraViewsAndPoints]):
     def _project_points(
         self,
         world_points: torch.Tensor,
-        camera_extrinisics: torch.Tensor,
+        camera_translations: torch.Tensor,
+        camera_rotations: torch.Tensor,
         camera_intrinisics: torch.Tensor,
     ) -> tuple[torch.Tensor, torch.Tensor]:
+        camera_translations = torch.cat([
+            torch.zeros()
+            camera_translations,
+        ], dim=0)
+        camera_rotations = torch.cat()
+        camera_relative_points = get_camera_relative_points(
+            world_points=world_points[],
+            camera_translations=camera_translations,
+            camera_rotations=camera_rotations,
+        )
+        homogen
+
         augmented_points = torch.cat(
             [world_points, torch.ones_like(world_points[:, 0:1])], dim=1
         )
@@ -195,34 +199,3 @@ class CameraAndParametersDataset(Dataset[CameraViewsAndPoints]):
             torch.cat([projected_u.unsqueeze(-1), projected_v.unsqueeze(-1)], dim=-1),
             visibility,
         )
-
-    def _generate_transform(self) -> torch.Tensor:
-        transform_parameters = torch.randn(6) * self._transform_std
-        cos_angles = torch.cos(transform_parameters[3:6])
-        sin_angles = torch.sin(transform_parameters[3:6])
-        rot_x = torch.tensor(
-            [
-                [1.0, 0.0, 0.0],
-                [0.0, cos_angles[0], -sin_angles[0]],
-                [0.0, sin_angles[0], cos_angles[0]],
-            ]
-        )
-        rot_y = torch.tensor(
-            [
-                [cos_angles[1], 0.0, -sin_angles[1]],
-                [0, 1.0, 0.0],
-                [sin_angles[1], 0.0, cos_angles[1]],
-            ]
-        )
-        rot_z = torch.tensor(
-            [
-                [cos_angles[1], -sin_angles[1], 0.0],
-                [sin_angles[1], cos_angles[1], 0.0],
-                [0.0, 0, 1.0],
-            ]
-        )
-        transform = torch.zeros(1, 4, 4)
-        transform[0, 0:3, 0:3] = rot_x @ rot_y @ rot_z
-        transform[0, 0:3, 3] = transform_parameters[0:3]
-        transform[0, 3, 3] = 1.0
-        return transform
